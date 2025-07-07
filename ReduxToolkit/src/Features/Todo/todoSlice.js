35050d47152db2d21f3c422bbb77a51a51519deb
mahmedsaleem1 <mahmedsaleem786@gmail.com>
2025-07-07 15:59:43 +0500

    feat: add updateTodo functionality to manage todo edits

--- ReduxToolkit/src/Features/Todo/todoSlice.js
+++ ReduxToolkit/src/Features/Todo/todoSlice.js
@@ -25,10 +25,18 @@ export const todoSlice = createSlice({
         },  
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
-        }    // In context we declare but in Redux define too
+        },    // In context we declare but in Redux define too
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => {
+                if (todo.id === action.payload.id) {
+                    todo.text = action.payload.text
+                }
+                return todo
+            })
+        }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
 
 export default todoSlice.reducer // Registering reducers to Store
--- ReduxToolkit/src/components/Todos.jsx
+++ ReduxToolkit/src/components/Todos.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux"
-import {removeTodo} from "../Features/Todo/todoSlice"
+import {removeTodo, updateTodo} from "../Features/Todo/todoSlice"
 
 const Todos = () => {
     const todos = useSelector((state) => state.todos)  // To retreive
@@ -17,6 +17,16 @@ const Todos = () => {
               onClick={() => dispatch(removeTodo(todo.id))}
             >
               Delete
+            </button>
+            <button
+              onClick={() => {
+                const newText = prompt("Edit todo:", todo.text)
+                if (newText) {
+                  dispatch(updateTodo({id: todo.id, text: newText}))
+                }
+              }}
+            >
+              Edit
             </button>
           </li>
         ))
